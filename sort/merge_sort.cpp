8d0bc81a0301610e3891a1f7cc7cb797261170be
Yusong Wu <1391530737@qq.com>
2019-01-21 16:18:11 +0800

    Update merge_sort.cpp

--- sort/merge_sort.cpp
+++ sort/merge_sort.cpp
@@ -1,3 +1,89 @@
+// 不需要temp数组的版本
+// 以及非递归的版本
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void merge(int a[], int ls, int mid, int re){
+	int lsize = mid - ls + 1;
+	int rsize = re - mid;
+	int rs = mid + 1;
+	int i, j, k;
+	
+	int larr[lsize];
+	int rarr[rsize];
+	
+	for(i = 0; i < lsize; i++){
+		larr[i] = a[i + ls];
+	}
+	for(j = 0; j < rsize; j++){
+		rarr[j] = a[j + rs];
+	}
+	
+	i = j = 0;
+	k = ls;
+	while(i < lsize && j < rsize){
+		if(larr[i] <= rarr[j]){
+			a[k++] = larr[i++];
+		}else{
+			a[k++] = rarr[j++];
+		}
+	}
+	
+	while(i < lsize) a[k++] = larr[i++];
+	while(j < rsize) a[k++] = rarr[j++]; 
+}
+
+void mergesort(int a[], int l, int r){
+	if(l < r){
+		int mid = l + (r - l) / 2;
+		mergesort(a, l, mid);
+		mergesort(a, mid + 1, r);
+		merge(a, l, mid, r);
+	}
+}
+
+void iter_mergesort(int a[], int l, int r){
+	// i表示每次merge的数组大小，i取1，2，4，8...
+	// j表示每次merge的数组最左边（即第一个元素）的下标 
+	int i, j; 
+	int tot = r - l + 1;
+	// i最多可以取到tot-1，是因为如果待排序数组的元素个数为奇数，就会出现类似下面的情况：
+	// o o o o o
+	// (oo) (oo) o
+	// (oooo) o
+	// (ooooo)
+	// 可见i最多能取到tot-1
+	// 而个数为偶数的情况下，两边的个数是一致的，i最多取tot/2
+	// 而j最大的取值是tot-2，即刚开始按照i=1来merge时，最后一轮的j是最大的
+	// 往后随着需要merge的数组大小在不断增加，j只能越变越小 
+	for(i = 1; i <= tot - 1; i *= 2){
+		for(j = 0; j <= tot - 2; j += 2*i){
+			int mid = j + i - 1;
+			int re = min(j + 2*i - 1, tot - 1);  // 在偶数的情况下，right end不可能取到tot - 1，奇数的情况下，只能取tot-1 
+			merge(a, j, mid, re);
+		}
+	}
+	
+}
+ 
+int main(){
+	int a[6] = {1, 0, -1, 5, 4, -3};
+	int temp[6];
+	
+	iter_mergesort(a, 0, 5);
+	
+	for(int i = 0; i < 6; i++)
+		cout << a[i] << " ";
+	
+	return 0;
+	
+} 
+
 // 下面是Weiss数据结构书上的版本
 void MergeSort(int A[],int N){
     int* tmpArray;
