103a1350f183b32b5fb86fdd21650cc00a441e9f
Yusong Wu <1391530737@qq.com>
2019-03-11 20:21:25 +0800

    Update avl tree.py

--- tree/avl tree.py
+++ tree/avl tree.py
@@ -1,5 +1,5 @@
 # https://blog.csdn.net/Magic1an/article/details/78881665
-# https://zhuanlan.zhihu.com/p/34899732
+# https://www.geeksforgeeks.org/avl-tree-set-2-deletion/
 class TreeNode(object):
     def __init__(self, x):
         self.left = None
@@ -12,10 +12,14 @@ class AVLTree(object):
     def __init__(self):
          self.root = None
     
-         
+    # Private Methods     
     def __get_height(self, node):
         return node.height if node != None else -1
     
+    
+    def __get_balance_factor(self, node):
+        return self.__get_height(node.left) - self.__get_height(node.right)
+        
     
     def __single_rotate_with_left(self, k2):
         """
@@ -98,36 +102,134 @@ class AVLTree(object):
             pass
         p.height = max(self.__get_height(p.left), self.__get_height(p.right)) + 1 
         return p
-
     
-    def __pre(self, node):
+    
+    def __find_min(self, p):
+        if p is None:
+            return None
+        else:
+            if p.left is None:
+                return p
+            else:
+                return self.__find_min(p.left)
+        
+    
+    def __delete(self, x, p):
+        # first perform normal BST deletion
+        if p is None:
+            return None
+        elif x < p.val:
+            p.left = self.__delete(x, p.left)
+        elif x > p.val:
+            p.right = self.__delete(x, p.right)
+        else:
+            if p.left is None:
+                tmp = p.right
+                p = None
+                return tmp
+            elif p.right is None:
+                tmp = p.left
+                p = None
+                return tmp
+            # p has 2 children
+            min_child = self.__find_min(p.right)
+            p.val = min_child.val
+            p.right = self.__delete(x, p.right)
+        
+        # if only a single node, return it    
+        if p is None:
+            return p
+        # update height
+        p.height = max(self.__get_height(p.left), self.__get_height(p.right)) + 1
+        # re-balance
+        balance_factor = self.__get_balance_factor(p)
+        # 4 conditions
+        # LL
+        if balance_factor >= 2 and self.__get_balance_factor(p.left) >= 0:
+            return self.__single_rotate_with_left(p)
+        # RR
+        if balance_factor <= -2  and self.__get_balance_factor(p.right) <= 0:
+            return self.__single_rotate_with_right(p)
+        # LR
+        if balance_factor >= 2 and self.__get_balance_factor(p.left) < 0:
+            return self.__double_rotate_with_left(p)
+        # RL
+        if balance_factor <= -2 and self.__get_balance_factor(p.left) > 0:
+            return self.__double_rotate_with_right(p)
+        return p
+    
+    
+    def __pre_order(self, node):
         if node:
             print(node.val)
-            self.__pre(node.left)
-            self.__pre(node.right)
+            self.__pre_order(node.left)
+            self.__pre_order(node.right)
     
+    
+    def __post_order(self, node):
+        if node:
+            self.__post_order(node.left)
+            self.__post_order(node.right)
+            print(node.val)
+    
+    
+    def __in_order(self, node):
+        if node:
+            self.__in_order(node.left)
+            print(node.val)
+            self.__in_order(node.right)
+    
+    
+    def __level_order(self, node):
+        queue = [node,]
+        while queue:
+            tot = len(queue)
+            level = []
+            for i in range(tot):
+                cur = queue.pop(0)
+                level.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            print(level)
+            
+            
+    # Public Methods
+    def pre_order(self):
+        self.__pre_order(self.root)
+    
+    
+    def post_order(self):
+        self.__post_order(self.root)
+    
+    
+    def in_order(self):
+        self.__in_order(self.root)
+    
+    
+    def level_order(self):
+        self.__level_order(self.root)
+        
             
     def insert(self, val):
-        self.root=self.__insert(val, self.root)
+        self.root = self.__insert(val, self.root)
         return self.root
     
     
-    def show(self):
-        self.__pre(self.root)
+    def delete(self, val):
+        self.root = self.__delete(val, self.root)
+        return self.root
 
 
+# some tests
 root = AVLTree()
-root.insert(3)
-root.insert(4)
-root.insert(5)
-root.insert(6)
-root.insert(1)
-root.insert(7)
-root.insert(9)
-root.insert(10)
-root.insert(11)
-root.insert(12)
-root.insert(22)
-root.show()
+nums = [9, 5, 10, 0, 6, 11, -1, 1, 2] 
+  
+for num in nums: 
+    root.insert(num)
+
+root.delete(10)  # should cause a LL rotation   
+root.pre_order() # should be 1 0 -1 9 5 2 6 11
 
 
