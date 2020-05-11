e9affd31b482d5433a09e75fe9fbeb5477d6dae1
Yusong Wu <1391530737@qq.com>
2020-05-11 10:39:22 -0700

    Update another-contest-template.cpp

--- another-contest-template.cpp
+++ another-contest-template.cpp
@@ -5,14 +5,21 @@ static const int _ = []() {
     return 0;
 }();
 
+typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
-typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+
 #define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) 
 #define FORN(a, b, c) for (int(a) = (b); (a) <= (c): ++(a))
 #define rep(i, n) FOR(i, 0, n)
 #define repn(i, n) FORN(i, 1, n)
 
+#define SORT(v) (sort(v.begin(), v.end()))
+
+#define sz(v) ((int)v.size())
+ 
 #define fi first 
 #define se second 
 #define mp make_pair 
