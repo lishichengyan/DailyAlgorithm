008a8245cdb12d37bc3853b81b9920070c0f00d5
Yusong Wu <1391530737@qq.com>
2020-05-11 09:01:00 -0700

    Create another-contest-template.cpp

---
+++ another-contest-template.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize("Ofast")
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef long long ll;
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) 
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c): ++(a))
+#define rep(i, n) FOR(i, 0, n)
+#define repn(i, n) FORN(i, 1, n)
+
+#define fi first 
+#define se second 
+#define mp make_pair 
+#define pb push_back 
