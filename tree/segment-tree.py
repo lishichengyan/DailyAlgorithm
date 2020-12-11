c0a34d3612120f27a4ffaa8a3c3c6bc50bbdbf53
Yusong Wu <1391530737@qq.com>
2020-12-10 23:13:22 -0800

    Update segment-tree.py

--- tree/segment-tree.py
+++ tree/segment-tree.py
@@ -1,3 +1,59 @@
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.arr = nums[:]
+        m = len(self.arr)
+        self.tree = [0] * (4*m+1)
+        self.__build(1, 0, m-1)
+        
+    def __build(self, pos, low, high):
+        """
+        pos: root of current subtree
+        low: lower bound, inclusive
+        high: upper bound, inclusive
+        """
+        if low > high:
+            return
+        if low == high:
+            self.tree[pos] = self.arr[low]
+            return
+        mid = (low + high) // 2
+        self.__build(2*pos, low, mid)
+        self.__build(2*pos+1, mid+1, high)
+        self.tree[pos] = self.tree[2*pos] + self.tree[2*pos+1]
+        
+    def __query(self, pos, i, j, low, high):
+        """
+        i: lower bound of the query, inclusive
+        j: upper bound of the query, inclusive
+        """
+        if i <= low and j >= high:
+            return self.tree[pos]
+        if i > high or j < low:
+            return 0
+        mid = (low + high) // 2
+        return self.__query(2*pos, i, j, low, mid) + self.__query(2*pos+1, i, j, mid+1, high)
+    
+    def __update(self, i, new_val, low, high, pos):
+        """
+        i: which position to update
+        """
+        if i < low or i > high:
+            return
+        if low == high:
+            self.tree[pos] = new_val
+            self.arr[i] = new_val
+            return
+        mid = (low + high) // 2
+        self.__update(i, new_val, low, mid, 2*pos)
+        self.__update(i, new_val, mid+1, high, 2*pos+1)
+        self.tree[pos] = self.tree[2*pos] + self.tree[2*pos+1]
+
+    def update(self, i: int, val: int) -> None:
+        return self.__update(i, val, 0, len(self.arr)-1, 1)
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.__query(1, i, j, 0, len(self.arr)-1)
+      
 class SegmentTree:
   IMAX = float("inf")
 
